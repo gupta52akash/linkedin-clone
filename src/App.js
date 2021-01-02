3fcdfaf3ae1c5190398cd1ce89d3f8941a36cde7
Akash Gupta <gupta52akash@hotmail.com>
2021-01-02 15:44:55 -0500

    feature[LC-26]: updated homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
+import Header from "./app/components/Header/Header";
+import Home from "./app/pages/Home/Home";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-		Hello World!
-		  {/* TODO: [LC-7]Header */}
-		  {/*		TODO: [LC-13] Logo */}
-		  {/*		TODO: [LC-14] search bar */}
-		  {/*		TODO: [LC-15] navbar icons */}
-		  {/* TODO: [LC-2]Main */}
-		  {/*	TODO: [LC-8]Sidebar */}
-		  {/*		TODO: [LC-16] personal details */}
-		  {/*		TODO: [LC-17] LinkedIn stats
-		  {/*		TODO: [LC-18] Recent updates */}
-		  {/*	TODO: [LC-9] feed */}
-		  {/*		TODO: [LC-5] inputContainer */}
-		  {/*		TODO: [LC-12]posts */}
-		  {/*	TODO: [LC-10]Widgets */}
-		  {/*		TODO: [LC-22]LinkedIn News */}
-		  {/*		TODO: [LC-23]Top viewed Courses */}
-		  {/*		TODO: [LC-24]Footer */}
+		<Header></Header>
+		 <Home></Home>
 	</div>
   );
 }
