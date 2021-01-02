21a010ad3599f252cff4bc414beb8187e600f862
Akash Gupta <gupta52akash@hotmail.com>
2021-01-02 15:43:42 -0500

    feature[LC-26]: added feeds component

---
+++ src/app/components/Feeds/Feeds.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feeds.css";
+
+function Feeds() {
+  return (
+    <div className="feeds">
+      {/*	TODO: [LC-9] feed */}
+      {/*		TODO: [LC-5] inputContainer */}
+      {/*		TODO: [LC-12]posts */}
+      This is feeds
+    </div>
+  );
+}
+
+export default Feeds;
