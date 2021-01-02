3ce9a71a707b304a5038d28bfd5d5e208317747b
Akash Gupta <gupta52akash@hotmail.com>
2021-01-02 15:43:58 -0500

    feature[LC-26]: Added sidebar component

---
+++ src/app/components/SideBar/SideBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SideBar.css";
+
+function SideBar() {
+  return (
+    <div className="sidebar">
+      {/*	TODO: [LC-8]Sidebar */}
+      {/*		TODO: [LC-16] personal details */}
+      {/*		TODO: [LC-17] LinkedIn stats */}
+      {/*		TODO: [LC-18] Recent updates */}
+      This is a sidebar
+    </div>
+  );
+}
+
+export default SideBar;
